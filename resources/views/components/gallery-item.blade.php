1fd094450f427737ca85b79dcccbd3ae034e28a6
Alex Noppenberger <noppenbe@gmx.de>
2023-11-05 19:45:14 +0100

    Update gallery-item.blade.php

--- resources/views/components/gallery-item.blade.php
+++ resources/views/components/gallery-item.blade.php
@@ -1,5 +1,6 @@
 <div class="p-5 w-fit">
     <div id="file_{{$pic}}" class="p-4 bg-zinc-900 flex items-center ">
+        {{ Storage::url("gallery/test/") }}{{$pic}}
         <img src="{{ Storage::url("gallery/test/") }}{{$pic}}" alt="Image" class="img w-[756px] rounded-xl shadow-xl">
     </div>
     <div class="p-4 text-white">{{$content}}</div>
