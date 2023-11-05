81a1bb12b8c3f1ef5a0157b3395006ff833521eb
Alex Noppenberger <noppenbe@gmx.de>
2023-11-05 19:29:43 +0100

    Update GalleryController.php

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -58,7 +58,7 @@ class GalleryController extends Controller
 
     public function store(Request $request){
         
-        $path = "app/public/gallery/test";
+        $path = "gallery/test";
         $request->validate([
             #'file' => 'required|max:2048',
             'file' => 'required',
