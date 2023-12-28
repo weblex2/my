4a0dacf432fc0285a902f9bdc191caca597a2395
Alex Noppenberger <noppenbe@gmx.de>
2023-12-28 21:15:31 +0100

    Update GalleryController.php

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -427,7 +427,7 @@ class GalleryController extends Controller
             die();
         } */
 
-        $files  = glob('./public/ffmpeg/*');
+        $files  = glob('./ffmpeg/*');
         dump($files);
         die();
         
