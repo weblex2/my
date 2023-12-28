67441f283cdaef86f4ee937245da27fcb8a3505f
Alex Noppenberger <noppenbe@gmx.de>
2023-12-28 21:22:50 +0100

    Update laravel-ffmpeg.php

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -2,13 +2,13 @@
 
 return [
     'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', 'ffmpeg/ffmpeg'),
+        'binaries' => env('FFMPEG_BINARIES', 'ffmpeg'),
 
         'threads' => 12,   // set to false to disable the default 'threads' filter
     ],
 
     'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', 'ffmpeg/ffprobe'),
+        'binaries' => env('FFPROBE_BINARIES', 'ffprobe'),
     ],
 
     'timeout' => 3600,
