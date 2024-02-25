c09c20817e7ec909074e6633666b8d49320fb683
Alex Noppenberger <noppenbe@gmx.de>
2024-02-25 16:00:17 +0100

    Update yt2mp3.php

--- app/Http/Controllers/yt2mp3.php
+++ app/Http/Controllers/yt2mp3.php
@@ -29,7 +29,7 @@ class yt2mp3 extends Controller
                 ->downloadPath('mp3')
                 ->extractAudio(true)
                 ->verbose(true)
-                ->audioFormat('mp3')
+                ->audioFormat('/var/www/html/my/mp3')
                 ->audioQuality('0') // best
                 ->output('%(title)s.%(ext)s')
                 ->url('https://www.youtube.com/watch?v=oDAw7vW7H0c')
