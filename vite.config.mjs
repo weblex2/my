8b2ee67c17af75e351daff75df3a9763d947c873
Alex Noppenberger <noppenbe@gmx.de>
2024-03-30 13:31:28 +0100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/kb.css',
+                'resources/css/gallery.css',
                 'resources/js/app.js',
             ],
             refresh: true,
