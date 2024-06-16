23d46c9a3d216cea7293b225b3b18dc8a69e9503
Alex Noppenberger <noppenbe@gmx.de>
2024-06-16 02:25:21 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
                 'resources/css/yt2mp3.css',
                 'resources/js/app.js',
                 'resources/js/noppal.js',
-                'resources/js/noppal.css',
+                'resources/css/noppal.css',
             ],
             refresh: true,
         }),
