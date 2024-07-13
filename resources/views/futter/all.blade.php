8c8edd194e02577a2dcd7aeae76d98ab06bac2e0
Alex Noppenberger <noppenbe@gmx.de>
2024-07-14 00:48:58 +0200

    Update all.blade.php

--- resources/views/futter/all.blade.php
+++ resources/views/futter/all.blade.php
@@ -11,7 +11,7 @@
             <div class="grid grid-cols-3 w-full mb-4 nobr items-justify text-center">
                 @foreach ($futter as $f)
                     <div class="food justify-center items-center mb-5" id={{$f->id}}> 
-                        <div class="mb-5">{{$f->name}}</div>
+                        <div class="food-name mb-5">{{$f->name}}</div>
                         <div class="relative flex justify-center items-center">
                             <div class="check absolute top-0 right-28 text-grey-500 hover:text-green-500"><i class="fa-solid fa-check"></i></div>
                             <img src='{{url('storage/futter/'.$f->img)}}' class="rounded-full w-52 h-52">
@@ -24,10 +24,11 @@
 </div>
 <script type="text/javascript">
     $('.check').click(function(){
-        alert("Ho");
+        var foodname = $(this).find('.food-name').val();
+        alert(foodname);
         $.ajax({
                 type: 'GET',
-                url: '{{route("ntfy.index",["msg" => "Hi2"])}}',
+                url: '{{route("ntfy.index",["msg" => '+ foodname+'])}}',
                 success: function (data){
                     console.log("message sent.");
                 },
