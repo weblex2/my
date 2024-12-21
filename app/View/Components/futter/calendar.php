e33b6fa8de4ec8340ff1af45167ed1d790dfe1a4
Alex Noppenberger <noppenbe@gmx.de>
2024-12-21 17:54:19 +0100

    Update calendar.php

--- app/View/Components/futter/calendar.php
+++ app/View/Components/futter/calendar.php
@@ -21,7 +21,8 @@ class calendar extends Component
 
     public function __construct($date, $ft)
     {   
-        dd($date);
+        //dd($date);
+        $this->datesDB = [];
         $this->ft = $ft;
         $startDate = Carbon::createFromFormat('Y-m-d', $date);
         $startDateDb = Carbon::createFromFormat('Y-m-d', $date);
