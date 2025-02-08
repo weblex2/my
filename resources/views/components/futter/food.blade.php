09a73af037130eff717b30a4155268eb5b97886c
Alex Noppenberger <noppenbe@gmx.de>
2025-02-08 16:53:43 +0100

    Update food.blade.php

--- resources/views/components/futter/food.blade.php
+++ resources/views/components/futter/food.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="food justify-center items-center mb-5" id="food_{{$food->id}}"> 
-        <div class="food-name text-2xl mb-5">{{$food->name}}</div>
+        <div class="food-name text-2xl h-3 mb-5">{{$food->name}}</div>
             <div class="relative flex justify-center items-center">
                 <div class="check absolute top-0 ml-48 text-grey-500 ">
                     <i class="fa-solid fa-check rounded-full p-2 hover:cursor-pointer hover:text-green-500 hover:bg-gray-500"></i> 
