0c0275ab516828c94fc0cdcfb03ccf1ec3b6a532
Alex Noppenberger <noppenbe@gmx.de>
2025-02-28 22:20:33 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,12 +7,13 @@
         </div>
         <div class="my-flex-box">
             <a href="/blog" class="box"><div >Laravel Blog</div></a>
-            <a href="/blog/cat/1" class="box"><div>Projekte</div></a>
+            <a href="/admin" class="box"><div>Filament</div></a>
             <a href="/travel-blog" class="box"><div>Reise Blog</div></a>
         </div>
         <div class="my-flex-box">
             <a href="/cv" class="box"><div>Lebenslauf</div></a>
             <a href="/futter" class="box"><div>Futter</div></a>
+            <a href="/notify/test" class="box"><div>Ntfy</div></a>
             <!-- <a href="/chat?userid=25" class="box"><div>Chat (Reverb)</div></a> -->
         </div>    
     </div>
