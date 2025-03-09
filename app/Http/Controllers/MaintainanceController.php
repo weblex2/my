e1c460f3b1527a44694d5c3d77951ea2e15febc2
Alex Noppenberger <noppenbe@gmx.de>
2025-03-09 13:37:48 +0100

    Update MaintainanceController.php

--- app/Http/Controllers/MaintainanceController.php
+++ app/Http/Controllers/MaintainanceController.php
@@ -10,21 +10,28 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 class MaintainanceController extends Controller
 {
     function backupDatabase()
-{
-    $containerName = 'mysql8'; // Name deines Docker-Containers
-    $dbUser = env('DB_USERNAME', 'root');
-    $dbPassword = "!Cyberbob03"; #env('DB_PASSWORD', 'secret');
-    $dbName = env('DB_DATABASE', 'laravel');
-    $backupPath = storage_path('app/laravel.sql');
+    {
 
-    $command = "docker exec $containerName mysqldump -u $dbUser -p$dbPassword $dbName > $backupPath";
-    echo "<pre>". $command."<br>";
-    $process = Process::fromShellCommandline($command);
-        try {
-            $process->mustRun();
-            return "Backup erfolgreich erstellt: " . $backupPath;
-        } catch (ProcessFailedException $exception) {
-            return "Fehler beim Backup: " . $exception->getMessage();
+        die();
+        $containerName = 'mysql8'; // Name deines Docker-Containers
+        $dbUser = env('DB_USERNAME', 'root');
+        $dbPassword = env('DB_PASSWORD', '!Cyberbob03');
+        $dbName = env('DB_DATABASE', 'laravel');
+        $backupPath = storage_path('app/laravel.sql');
+
+        $command = "docker exec $containerName mysqldump -u $dbUser -p$dbPassword $dbName > $backupPath";
+        echo "<pre>". $command."<br>";
+        $process = Process::fromShellCommandline($command);
+            try {
+                $process->mustRun();
+                $this->uploadToS3($backupPath);
+                return "Backup erfolgreich erstellt: " . $backupPath;
+            } catch (ProcessFailedException $exception) {
+                return "Fehler beim Backup: " . $exception->getMessage();
+            }
         }
-    }
+
+        public function uploadToS3($backupPath){
+            $res = Storage::disk('s3')->put('sql_backup/laravel_'.date('Ymd').".sql", file_get_contents($backupPath));
+        }
 }
