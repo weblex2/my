2724757573151be3272614149badbd57e78f6d6a
Alex Noppenberger <noppenbe@gmx.de>
2025-03-15 14:36:37 +0100

    SSC

--- resources/views/layouts/ssc.blade.php
+++ resources/views/layouts/ssc.blade.php
@@ -8,26 +8,26 @@
         <title>{{ config('app.name', 'SCC') }}</title>
 
         <!-- Fonts -->
-        
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism.min.css" rel="stylesheet" />
-        
+
         <!-- <script src="https://code.jquery.com/jquery-3.1.1.min.js"> -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>    
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <!-- Prism.js Core -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism.min.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-core.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/autoloader/prism-autoloader.min.js"></script>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css" rel="stylesheet" />
-        
+
         <!-- Scripts -->
         @vite(['resources/css/ssc.css'])
- 
+
         <!-- Styles -->
         @livewireStyles
-        
+
     </head>
     <body>
         @livewireScripts
@@ -37,5 +37,5 @@
     <script>
           hljs.highlightAll();
     </script>
-    
+
 </html>
--- resources/views/showSourceCode/index.blade.php
+++ resources/views/showSourceCode/index.blade.php
@@ -1,58 +1,59 @@
 @extends('layouts.ssc')
 @section('content')
-<div class="loader"><img src="loader.gif"></div>
+<div class="loader"><img class="w-12 h-12" src="{{asset("img/loading6.webp")}}"></div>
+<div class="wrapper">
 <div class="container" >
     <div class="showCodeFiles">
-        
+
         @foreach($structure as $type => $typeRecords)
-            <div class="laravel-type">{{ucfirst($type)}}</div> 
+            <div class="laravel-type">{{ucfirst($type)}}</div>
             @foreach ($typeRecords as $item)
             <div class="laravel-type-wrapper">
                 <div class="p-1 m-0">
                     <div>
                         @if ($item['type']=="folder")
                             <div class="D">
-                                {!! str_repeat('&nbsp;', $item['depth']*4) !!} 
-                                <span class="dir"> 
-                                    <i class="dir-icon"></i>    
+                                {!! str_repeat('&nbsp;', $item['depth']*4) !!}
+                                <span class="dir">
+                                    <i class="dir-icon"></i>
                                     {!! $item['file']  !!}
                                 </span>
                             </div>
                         @elseif ($item['type']=="file")
                             <div>
                                 <span class="file {{ str_replace("/","\\", $item['path']) == str_replace("/","\\",$startFile) ? "file-active" : ""  }}" path="{{ $item['path']  }}" title="{{ $item['path'] }}">
-                                    {!! str_repeat('&nbsp;', $item['depth']*4) !!} 
-                                    <i class="fa-regular fa-file text-gray-300"></i> 
+                                    {!! str_repeat('&nbsp;', $item['depth']*4) !!}
+                                    <i class="text-gray-300 fa-regular fa-file"></i>
                                     {!! $item['file']  !!}
                                 </span>
                             </div>
                         @endif
-                    </div>    
+                    </div>
                 </div>
             </div>
              @endforeach
         @endforeach
-    
+
     </div>
-    
+
     <div class="showCodeContent">
         <pre><code id="code-block" class="language-php">{{$startFileContent}}</code></pre>
     </div>
 </div>
+</div>
 
 
-    
     <script>
         $().ready(function () {
             //$('.file-active').click();
 
             $('.file').click(function(){
-                //$('.loader').show();
-                var path = $(this).attr('path');   
+                $('.loader').css('display', 'flex');
+                var path = $(this).attr('path');
                 $.ajax({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },        
+                },
                 type: 'POST',
                 url: '{{ url("ssc/getCode") }}',
                 data: {'path' : path},
@@ -65,11 +66,11 @@
                     }
                     console.log(linecount);
                     const codeBlock = document.getElementById('code-block');
-                    
-                    
+
+
                     // Prism.js Highlighting anwenden
-                    
-                    
+
+
                     textArr = content.split("\n");
                     console.log("ta length: " + textArr.length);
                     console.log("buchstaben: " + (textArr.length).toString().length);
@@ -80,19 +81,21 @@
                     }
                     var textWithNumbers = textArr.join('\n');
                     codeBlock.textContent = textWithNumbers;
-                    
+
                     Prism.highlightElement(codeBlock);
-                    //$('.loader').hide();
+                    $('.loader').css('display', 'none');
                 },
                 error: function( data) {
                     console.log(data);
                     //$('.loader').hide();
+                    $('.loader').css('display', 'none');
                 }
+
             });
             });
         });
 
-        
-  
+
+
     </script>
 @endsection
