b0abbccce10ef4dd320aba92216bef8f87f1730e
Alex Noppenberger <noppenbe@gmx.de>
2025-03-16 16:20:25 +0100

    Update cv.blade.php

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -1,64 +1,65 @@
 <x-noppal>
 
-<div class="container mx-auto flex-auto pt-20">
+<div class="container flex-auto pt-20 mx-auto">
 
-    <div class="flex justify-center "> 
-   
-    <img src="{{asset('img/noppal3.jpg')}}" class="img-me">
+    <div class="flex flex-col items-center justify-center ">
+        <div><img src="{{asset('img/noppal3.jpg')}}" class="img-me"></div>
+        <div class="p-3 mt-5 border !border-zinc-700"><i class="fa-solid fa-download"></i> <a target="_blank" href="{{route("cv.pdf")}}"> [Download PDF]</a></div>
     </div>
     <br>
     <br>
-        <div class="cv grid grid-cols-2 gap-0 ">
+        <div class="grid grid-cols-2 gap-0 cv ">
             <h1 class="col-span-2 text-center">Lebenslauf</h1>
+
             {{-- Edit  = {{$edit}} --}}
             <hr class="col-span-2 mb-10">
             <div class="grid grid-cols-6">
-                <h2 class="col-span-6  border-red-500">Persönliche Daten</h2>
+                <h2 class="col-span-6 ">Persönliche Daten</h2>
                 @foreach ($data['PD'] as $key => $pd)
-                    <div class="header col-span-2">
+                    <div class="col-span-2 header">
                         {{$pd->name}}
                     </div>
-                    <div class="bl_text mb-5  col-span-4">
+                    <div class="col-span-4 mb-5 bl_text">
                         {!!nl2br($pd['value'])!!}
                     </div>
                 @endforeach
 
                 <div class="col-span-6">
                     <hr class="mb-5">
-                </div>    
+                </div>
 
                 <div class="col-span-6">
                     <h2>Ausbildung</h2>
                 </div>
-                
+
                 @foreach ($data['AU'] as $key => $pd)
-                    
+
                      @php
                         if ($pd->date_to=="0000-00-00"){
                             $pd->date_to = "jetzt";
                         }
                     @endphp
-                    <div class="header col-span-6">{{$pd->date_from}} - {{$pd->date_to}}</div>
+                    <div class="col-span-6 header">{{$pd->date_from}} - {{$pd->date_to}}</div>
                     <h3 class="col-span-6">{{$pd->header}}</h3>
-                    <div class="bl_text mb-10  col-span-6">
+                    <div class="col-span-6 mb-10 bl_text">
                         {!!nl2br($pd['value'])!!}
                     </div>
                 @endforeach
-                
+
                 <div class="col-span-6">
                     <hr class="mb-5">
-                </div> 
+                </div>
 
                 <div class="col-span-6">
                     <h2>Kenntnisse & Fähigkeiten</h2>
-                </div>  
+                </div>
 
                 @foreach ($data['LANG'] as $key => $kf)
                     <h3 class="col-span-1">{{$kf->header}}</h3>
-                    <div class="bl_text  col-span-5">
+                    <div class="col-span-5 bl_text">
                         {!!nl2br($kf['value'])!!}
                     </div>
-                @endforeach 
+                @endforeach
                 <div class="col-span-6">
                     <div class="my-10">
                     <span class="tag">PHP</span>
@@ -81,7 +82,7 @@
 
 
             </div>
-            <div class="grid grid-cols-1 row-span-12 ml-20">
+            <div class="grid grid-cols-1 ml-20 row-span-12">
                 <h2 class="col-span-2">Berufliche Laufbahn</h2>
                 @foreach ($data['BL'] as $key => $bl)
                 <div class="col-span-2 bl">
@@ -92,10 +93,10 @@
                         }
                     @endphp
                     <div class="header">{{$bl->date_from}} - {{$bl->date_to}}</div>
-                    <div class="bl_text mb-10">{!!nl2br($bl['value'])!!}</div>
+                    <div class="mb-10 bl_text">{!!nl2br($bl['value'])!!}</div>
                 </div>
                 @endforeach
-            </div>    
+            </div>
         </div>
         {{-- <button type="button" class="btn">Edit</button>
         <br> --}}
