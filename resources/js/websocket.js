d0c7f0f118aa35178ec8ab03e63ef5b536102cc8
Alex <noppenbe@gmx.de>
2025-03-18 10:04:23 +0100

    Update websocket.js

--- resources/js/websocket.js
+++ resources/js/websocket.js
@@ -32,7 +32,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
+    wsHost: 'noppal.de',
     wsPort: 9002,
     wssPort: 9002,
     forceTLS: true,
