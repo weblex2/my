2357eaab1ffde960fbc7a3072154ca3c61ab7d0f
Alex <noppenbe@gmx.de>
2025-03-19 16:39:03 +0100

    Update maintenance-layout.blade.php

--- resources/views/components/layouts/maintenance-layout.blade.php
+++ resources/views/components/layouts/maintenance-layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Dashboard' }}</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
     @vite(['resources/css/noppal.css'])
 </head>
 <body class="bg-gray-100">
