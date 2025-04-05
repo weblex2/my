88c034cf1933bb86cf59aa6ae5585b2b378bd41b
Alex Noppenberger <noppenbe@gmx.de>
2025-04-05 03:15:57 +0200

    Update migrationBadge.php

--- app/View/Components/LaravelMyAdmin/migrationBadge.php
+++ app/View/Components/LaravelMyAdmin/migrationBadge.php
@@ -7,15 +7,13 @@ use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 use App\Http\Controllers\LaravelMyAdminController;
 
-class migrationBadge extends Component
+class MigrationBadge extends Component
 {
     public $count;
-
 
     public function __construct()
     {
         $this->count = LaravelMyAdminController::getNewMigrationCount() ?? 0;
-        $this->count = 1;
     }
 
     /**
