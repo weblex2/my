4ceef9a9959354aa8c98771582a31091b99018c3
Alex Noppenberger <noppenbe@gmx.de>
2025-04-05 03:22:01 +0200

    Update migration-badge.blade.php

--- resources/views/components/laravel-my-admin/migration-badge.blade.php
+++ resources/views/components/laravel-my-admin/migration-badge.blade.php
@@ -1,3 +1,3 @@
 Migrations
 <span class="sr-only">Notifications</span>
-<div class="{{$count==0 ? "!hidden" : ""}} migration-badge">{{$count}}</div>
+<div class="{{$count==0 ? "!hidden" : ""}} migration-badge">{{-- {{$count}} --}}1</div>
