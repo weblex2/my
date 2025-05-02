21f78eb5270662bbcf9b2241802f3a002ea06a41
Alex Noppenberger <noppenbe@gmx.de>
2025-05-02 05:24:06 +0200

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -158,7 +158,8 @@ class AdminPanelProvider extends PanelProvider
     }
 
     public static function getLinks($counts){
-        $filters = FilamentConfig::where('type','navlink')->orderBy('order', 'asc')->get();
+        //$filters = FilamentConfig::where('type','navlink')->orderBy('order', 'asc')->get();
+        $filters = [];
         $navItems = [];
         foreach ($filters as $i => $filter){
             $name = ucfirst($filter->value);
