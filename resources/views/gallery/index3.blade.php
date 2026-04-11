417e391d796338982ab62143d3b33bf00ef98e6d
Alex Noppenberger <noppenbe@gmx.de>
2026-04-12 00:03:34 +0200

    Update index3.blade.php

--- resources/views/gallery/index3.blade.php
+++ resources/views/gallery/index3.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Travel Map ✈</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/travelmap.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <style>
         *,
